15e745416f49f88904ec22fce702f6325873f2b3
Aman Shankar <amanshankar80@gmail.com>
2023-02-17 01:22:32 +0530

    Login_Controller

---
+++ Project_1/Service/Controller/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Business_Logic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Service.Properties
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        IUserLogin_Logic _userLogic;
+        Validation _valid;
+        public LoginController(IUserLogin_Logic userLogic, Validation valid)
+        {
+            _userLogic = userLogic;
+            _valid = valid;
+        }
+        [HttpGet("Login")]
+        public ActionResult Login(string Email, string Password)
+        {
+            if (!_valid.CheckUserExists(Email, Password))
+            {
+                return Unauthorized("No Details Found");
+            }
+            else
+            {
+                return Ok("User Found");
+            }
+        }
+    }
+}
